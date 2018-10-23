968c531a30423f4cf173d113e3113f4f3732ae5b
AlexCabezas2018 <alexcabezas0705@gmail.com>
2018-10-23 12:15:54 +0200

    Añadido atributo type en TEmployee

--- src/business/employee/TEmployee.java
+++ src/business/employee/TEmployee.java
@@ -7,15 +7,17 @@ public class TEmployee {
 	private Float salary = null;
 	private Boolean active = null;
 	private Integer id_main_office = null;
+	private String type = null;
 
 	public TEmployee(){}
 	
-	public TEmployee(Integer id, String id_card_number, float salary, boolean active, Integer id_main_office) {
+	public TEmployee(Integer id, String id_card_number, float salary, boolean active, Integer id_main_office, String type) {
 		this.id = id;
 		this.id_card_number = id_card_number;
 		this.salary = salary;
 		this.active = active;
 		this.id_main_office = id_main_office;
+		this.type = type;
 	}
 
 	public Integer getId() {
@@ -57,4 +59,8 @@ public class TEmployee {
 	public void setId_main_office(Integer id_main_office) {
 		this.id_main_office = id_main_office;
 	}
+
+	public String getType() { return this.type; }
+
+	public void setType(String type) { this.type = type; }
 }
