878904145d3c345de8cb51b6763be3fae81dfa0c
Pablo Agudo Brun <paagudo@ucm.es>
2018-10-25 12:10:06 +0200

    Creada clase MainWindows

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,12 @@
+import presentation.MainWindow;
+
 import javax.swing.*;
-import java.awt.*;
 
-public class Main extends JFrame {
 
-	public Main() {
-		setTitle("GreenGO");
-		initGUI();
-	}
+public class Main {
 
-	private void initGUI() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage("resources/images/logo.jpg"));
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
-
-			/*
-			PanelTabs tabbedPane = new PanelTabs();
-			setContentPane(tabbedPane);
-			tabbedPane.actualizarMarca();
-			*/
-
-		pack();
-		setVisible(true);
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(MainWindow:: new);
 	}
 }
+
