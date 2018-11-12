c8994e00697232d7b004744e64d47f9f1eb11e13
Pablo Agudo Brun <paagudo@ucm.es>
2018-11-12 11:23:27 +0100

    Arreglado problemas en presentación.

--- src/presentation/rental/RentalTableModel.java
+++ src/presentation/rental/RentalTableModel.java
@@ -1,9 +1,10 @@
 package presentation.rental;
 
 import business.rental.TRental;
+import business.rental.TRentalDetails;
 import presentation.util.TableModel;
 
-public class RentalTableModel extends TableModel<TRental> {
+public class RentalTableModel extends TableModel<TRentalDetails> {
 
 	public RentalTableModel(String[] columnIds) {
 		super(columnIds);
@@ -14,25 +15,25 @@ public class RentalTableModel extends TableModel<TRental> {
 		Object s = null;
 		switch (indiceCol) {
 			case 0:
-				s = list.get(indiceFil).getId();
+				s = ((TRental)list.get(indiceFil).getRental()).getId();
 				break;
 			case 1:
-				s = list.get(indiceFil).getIdVehicle();
+				s = ((TRental)list.get(indiceFil).getRental()).getIdVehicle();
 				break;
 			case 2:
-				s = list.get(indiceFil).getIdClient();
+				s = ((TRental)list.get(indiceFil).getRental()).getIdClient();
 				break;
 			case 3:
-				s = list.get(indiceFil).isActive();
+				s = ((TRental)list.get(indiceFil).getRental()).isActive();
 				break;
 			case 4:
-				s = list.get(indiceFil).getNumKmRented();
+				s = ((TRental)list.get(indiceFil).getRental()).getNumKmRented();
 				break;
 			case 5:
-				s = list.get(indiceFil).getDateFrom();
+				s = ((TRental)list.get(indiceFil).getRental()).getDateFrom();
 			break;
 			case 6:
-				s = list.get(indiceFil).getDateTo();
+				s = ((TRental)list.get(indiceFil).getRental()).getDateTo();
 				break;
 			default:
 				assert (false);
